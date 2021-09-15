1ced3dd97e574224ae8fb797b84a7e6cb88cbcaf
Amr Ali <amr.alifciscu@gmail.com>
2021-09-15 09:45:20 +0200

    Initial Commit

---
+++ AmrAnimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmrAnimi
+//
+//  Created by Amr Ali on 15/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
